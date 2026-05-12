62c34779ab21a7a8333354bc1dcd1c2cc0b313f9
ashaapaww99 <shafamallyaa@gmail.com>
2026-05-12 09:11:07 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,5 +3,5 @@ import streamlit as st
 st.title("Welcome to Kalkulator 2026")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-    
+( 
 
